61faf506907007e5fe15f9c6e3bfe5b271562faa
Daniel Ursache Dogariu <contact@danniel.net>
2022-10-28 14:48:48 +0300

    Fix typo

--- tembaimporter/management/commands/tembaimport.py
+++ tembaimporter/management/commands/tembaimport.py
@@ -222,7 +222,7 @@ class Command(BaseCommand):
                 for guuid in contact_group_uuids[contact.uuid]:
                     gid = groups_uuid_id.get(uuid=guuid)
                     # Use the Django's "through" table and bulk add the contact_id + group_id pairs
-                    group_through_queue.append(Contact.groups.through(contact_id=contact.id, group_id=gid))
+                    group_through_queue.append(Contact.groups.through(contact_id=contact.id, contactgroup_id=gid))
             Contact.groups.through.objects.bulk_create(group_through_queue)
 
         return total            
