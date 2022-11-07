ff5598ab6054c11b4721b84e6b007da9b13d254f
Daniel Ursache Dogariu <contact@danniel.net>
2022-11-07 02:31:57 +0200

    Remove junk text

--- tembaimporter/management/commands/tembaimport.py
+++ tembaimporter/management/commands/tembaimport.py
@@ -306,5 +306,3 @@ class Command(BaseCommand):
                 creation_queue.append(item)
             total += len(Campaign.objects.bulk_create(creation_queue))
         return total            
-
-https://rapidpro-static-app.s3.amazonaws.com/99/message_D20211106_4e7009ace847a12c0ac4c1fef2674586.jsonl.gz?response-content-disposition=attachment%3B&response-content-type=application%2Foctet&response-content-encoding=none&AWSAccessKeyId=AKIAWSRWE3IH3URI3MPL&Signature=v5AUUuoQhyJYAJJ%2F3CoRR7G1o%2BM%3D&Expires=1667866922
